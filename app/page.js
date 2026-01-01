d62f3dfe676cb61a56d52585983559d9a6cb4909
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-01 14:02:02 +0000

    Fetching data in a Page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Home() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to paradise</h1>
 
       <Link href="/cabins">Explore cabins</Link>
