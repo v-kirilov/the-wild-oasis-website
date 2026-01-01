d8662fa7bdfd74faf45f712e24e0e288a7e1e58a
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-01 12:53:25 +0000

    Navigating between pages.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
+import Navigation from "./components/Navigation";
 
 export default function Home() {
   return (
-   <h1>The Wild Oasis. Welcome to paradise</h1>
+    <div>
+      <Navigation />
+      <h1>The Wild Oasis. Welcome to paradise</h1>
+
+      <Link href="/cabins">Explore cabins</Link>
+    </div>
   );
 }
