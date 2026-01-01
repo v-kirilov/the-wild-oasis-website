7314bf5694c30297c20180af54a41f3890984697
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-01 14:10:46 +0000

    ADding interactivity with client components.

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,8 +1,8 @@
+import Counter from "../components/Counter";
+
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
-
-  console.log(data);
 
   return (
     <div>
@@ -13,6 +13,7 @@ export default async function Page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <Counter users={data}/>
     </div>
   );
 }
