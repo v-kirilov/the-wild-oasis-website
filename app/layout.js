9ae082935783816d4864eeb18b60b09af7574a44
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-02 08:25:17 +0000

    Adding page metadata and favicon.

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,13 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
-  description: "Welcome to The Wild Oasis, your paradise getaway.",
+  //title: "The Wild Oasis",
+  title: {
+    template: "%s The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel located in the heart of Italian Dolomites, surrounded by breathtaking nature.",
 };
 
 export default function RootLayout({ children }) {
