7f3cdf00a5162170de7806ba1cb0016f75f17aa5
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-02 14:28:05 +0000

    Fetching and displaying cabin list.

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,14 @@
 import Counter from "@/app/_components/Counter";
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
 
   return (
     <div>
@@ -18,9 +19,9 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature&#39;s beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&#39;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
       {cabins.length > 0 && (
