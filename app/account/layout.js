3c9837dbf352f13ab38ebed3efb0de83b865ae91
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-02 13:52:35 +0000

    Adding a nested layout.

---
+++ app/account/layout.js
@@ -0,0 +1,10 @@
+import Navigation from "../_components/SideNavigation";
+
+export default function Layout({children}) {
+  return (
+    <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+      <Navigation />
+      <div className="py-1">{children}</div>
+    </div>
+  );
+}
