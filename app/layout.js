1ec64b7026cb92b3829aae1c39deb08c80aa7717
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-02 08:43:10 +0000

    Improving the navigation and root layout.

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
-import {Josefin_Sans} from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -21,13 +22,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by the Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl bg-red-500 mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
