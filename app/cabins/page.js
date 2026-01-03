227b8706a72fe3f1b86f78ddadbd4009d5ba1028
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-03 20:12:51 +0000

    Sharing state between client and server, the URL.

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,16 +1,21 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
-// revalidates the data cache
+// revalidates the data cache but if the page is dynamically redered than this is useless it does not apply
 export const revalidate = 3600;
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+//Since we use search params here , this means that in build time we don't know what are the params
+//meaning that the page will be now rendered dynamically and not statically
+export default async function Page({ searchParams }) {
   // CHANGE
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,8 +29,13 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
