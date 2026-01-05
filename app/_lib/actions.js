23da983d203a8d9e423c0255fe15311afa8da650
v-kirilov <v_kirilov87@protonmail.ch>
2026-01-05 12:47:02 +0000

    Building a Custom Sign Out Button.

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,7 +1,12 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 export async function signInAction() {
-  await signIn("google",{redirectTo: "/account"});
+  await signIn("google", { redirectTo: "/account" });
+}
+
+export async function signOutAction() {
+  // Sign out and redirect to homepage
+  await signOut({ redirectTo: "/" });
 }
